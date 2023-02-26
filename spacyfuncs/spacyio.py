4e009d8af982b6463fc622fe1eccc40f9e28cf22
langdonholmes <55119338+langdonholmes@users.noreply.github.com>
2023-02-26 17:13:47 -0600

    add keys namedtuple to convert

--- spacyfuncs/spacyio.py
+++ spacyfuncs/spacyio.py
@@ -1,6 +1,7 @@
 import warnings
 from pathlib import Path
 from typing import Iterable, List
+from collections import namedtuple
 
 import spacy
 import srsly
@@ -12,8 +13,9 @@ if not Doc.has_extension('name'):
     Doc.set_extension('name', default=None)
 nlp = spacy.blank('en')
 
-app = typer.Typer()
+JsonKeys = namedtuple('JsonKeys', ['id', 'text', 'labels'])
 
+app = typer.Typer()
 
 def _to_docbin(docs: Iterable[Doc], out_path: Path) -> None:
     '''Helper function that sends an iterable of docs to a DocBin on disk
@@ -23,18 +25,18 @@ def _to_docbin(docs: Iterable[Doc], out_path: Path) -> None:
         db.add(doc)
     db.to_disk(Path(out_path))
     
-def _convert(text_lines: Iterable[dict]) -> Iterable[Doc]:
+def _convert(text_lines: Iterable[dict], keys: JsonKeys) -> Iterable[Doc]:
     '''Convert entity annotation from Doccano format to spaCy v3 .spacy format.
     '''
     for line in text_lines:
-        text = line['data']
+        text = line[keys.text]
         doc = nlp.make_doc(text)
-        doc._.name = line['id']
+        doc._.name = line[keys.id]
         ents = []
-        for start, end, label in line['label']:
+        for start, end, label in line[keys.labels]:
             span = doc.char_span(start, end, label=label, alignment_mode="strict")
             if span is None:
-                msg = f"Document: {line['id']}\nEntity [{start}, {end}, {label}] does not align with token boundaries.\nOriginal entity was '{doc.text[start:end]}'"
+                msg = f"Document: {line[keys.id]}\nEntity [{start}, {end}, {label}] does not align with token boundaries.\nOriginal entity was '{doc.text[start:end]}'"
                 span = doc.char_span(start, end, label=label, alignment_mode="expand")
                 msg += f"\nAttempting to set entity as '{span}'"
                 warnings.warn(msg)
@@ -80,21 +82,31 @@ def get_docs(texts: Iterable = typer.Argument(..., help='An iterable of (name, t
 def convert(in_file: str = typer.Argument(..., help='The location of the jsonl file'),
             out_location: str = typer.Argument(..., help='Where to put the created DocBin/s'),
             train_percent: int = typer.Argument(0, help='The percentage of the data to use for training. Set to 0 to skip splitting data.'),
+            id_key: str = typer.Argument('id', help='Where to find the the document identifier.'),
+            text_key: str = typer.Argument('text', help='Where to find the document text.'),
+            labels_key: str = typer.Argument('label', help='Where to find the annotations.'),
             ) -> None:
     '''Converts a jsonl file to a train/dev/test split of .spacy files.
     '''
     from sklearn.model_selection import train_test_split
 
     input_path = Path(in_file)
-    
+        
     if input_path.suffix == '.jsonl':
         raw = list(srsly.read_jsonl(input_path))
-        if any([key not in raw[0].keys() for key in ['id', 'data', 'label']]):
-            warnings.warn("Raw data is expected to be json lines with id, data, label keys.")
+        
+        # store the keys in a named tuple for easy access
+        keys = JsonKeys(id_key, text_key, labels_key)
+        
+        if any([key not in raw[0].keys() for key in keys]):
+            warnings.warn("Raw data is expected to be json lines with {name_key, text_key, label_key} keys.}")
             return
-        docs = list(_convert(raw))
+        
+        docs = list(_convert(raw, keys))
+        
     elif input_path.suffix == '.spacy':
         docs = list(DocBin().from_disk(input_path).get_docs(nlp.vocab))
+        
     else:
         warnings.warn("Unknown filetype. '.spacy' and '.jsonl' are supported.")
         return
@@ -103,6 +115,7 @@ def convert(in_file: str = typer.Argument(..., help='The location of the jsonl f
     
     if train_percent == 0:
         _to_docbin(docs, out_path)
+        
     else:
         train, _remains = train_test_split(docs, train_size=train_percent/100, random_state=0)
         dev, test = train_test_split(_remains, train_size=0.5, random_state=0)
